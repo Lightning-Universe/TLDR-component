688b98e69181b66dc9d11eb88557f7b53a7bf013
awaelchli <aedu.waelchli@gmail.com>
2022-11-22 14:16:22 +0100

    fix requirements

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 # !pip install pandas numpy sentencepiece transformers==4.16.2 scikit-learn
-# !pip install torch>=1.7.0,!=1.8.0 lightning>=1.8.2,<1.8.3
+# !pip install 'torch>=1.7.0,!=1.8.0'
+# !pip install 'lightning>=1.8.2,<1.8.3'
 
 from typing import Any, Tuple
 import torch.nn as nn
