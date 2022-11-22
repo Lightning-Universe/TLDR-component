4a036eb34d9c913c7aa2d28b3a2e9afd4f72f435
awaelchli <aedu.waelchli@gmail.com>
2022-11-22 17:42:35 +0100

    imports

--- examples/text_summarization/app.py
+++ examples/text_summarization/app.py
@@ -1,17 +1,7 @@
-# !pip install pandas numpy sentencepiece transformers==4.16.2 scikit-learn
-# !pip install 'torch>=1.7.0,!=1.8.0'
-# !pip install 'lightning>=1.8.2,<1.8.3'
-# !pip install 'git+https://github.com/Lightning-AI/LAI-LLM-components'
-import os
-from typing import Any, Tuple
-import torch.nn as nn
+# !pip install 'git+https://github.com/Lightning-AI/LAI-TLDR'
 import lightning as L
-from lightning.app.components import LightningTrainerMultiNode
-from transformers import T5ForConditionalGeneration
-from transformers import T5TokenizerFast as T5Tokenizer
-
-from llm_components.text_summarization.text_summarization import predict
-from llm_components.text_summarization import TLDR
+from transformers import T5ForConditionalGeneration, T5TokenizerFast as T5Tokenizer
+from llm_components.text_summarization import predict, TLDR
 
 sample_text = """
 ML Ops platforms come in many flavors from platforms that train models to platforms that label data and auto-retrain models. To build an ML Ops platform requires dozens of engineers, multiple years and 10+ million in funding. The majority of that work will go into infrastructure, multi-cloud, user management, consumption models, billing, and much more.
@@ -21,7 +11,7 @@ Build your platform with Lightning and launch in weeks not months. Focus on the 
 
 class MyTLDR(TLDR):
 
-    def get_model(self) -> Tuple[nn.Module, Any]:
+    def get_model(self):
         t5 = T5ForConditionalGeneration.from_pretrained("t5-base", return_dict=True)
         t5_tokenizer = T5Tokenizer.from_pretrained("t5-base")
         return t5, t5_tokenizer
@@ -39,10 +29,8 @@ class MyTLDR(TLDR):
             print("Summarized text:\n", predictions[0])
 
 
-
-
 app = L.LightningApp(
-    LightningTrainerMultiNode(
+    L.app.components.LightningTrainerMultiNode(
         MyTLDR,
         num_nodes=2,
         cloud_compute=L.CloudCompute("gpu", disk_size=50),
--- llm_components/text_summarization/__init__.py
+++ llm_components/text_summarization/__init__.py
@@ -1,3 +1,4 @@
 from llm_components.text_summarization.tldr import TLDR
+from llm_components.text_summarization.text_summarization import predict
 
-__all__ = ["TLDR"]
+__all__ = ["TLDR", "predict"]
