177e36cfe0fce8cc88fdbd23a3a4acb5b19e8fd1
awaelchli <aedu.waelchli@gmail.com>
2022-11-22 17:44:51 +0100

    requirements

--- setup.py
+++ setup.py
@@ -1,2 +1,14 @@
 import setuptools
-setuptools.setup()
+setuptools.setup(
+    install_requires=[
+        'pandas',
+        "numpy",
+        "sentencepiece",
+        "transformers==4.16.2",
+        "scikit-learn",
+]
+
+)
+
+
+
