91f6dd06a8b8a17f761a39f69f504a7ded6ed1ce
Justus Schock <justus.schock@posteo.de>
2022-11-24 14:17:28 +0100

    fix imports

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 # !pip install 'git+https://github.com/Lightning-AI/LAI-TLDR'
 import lightning as L
 from transformers import T5ForConditionalGeneration, T5TokenizerFast as T5Tokenizer
-from lai_tldr.text_summarization import predict, TLDR
+from lai_tldr import predict, TLDR
 
 sample_text = """
 ML Ops platforms come in many flavors from platforms that train models to platforms that label data and auto-retrain models. To build an ML Ops platform requires dozens of engineers, multiple years and 10+ million in funding. The majority of that work will go into infrastructure, multi-cloud, user management, consumption models, billing, and much more.
@@ -12,8 +12,9 @@ Build your platform with Lightning and launch in weeks not months. Focus on the 
 class MyTLDR(TLDR):
 
     def get_model(self):
-        t5 = T5ForConditionalGeneration.from_pretrained("t5-base", return_dict=True)
-        t5_tokenizer = T5Tokenizer.from_pretrained("t5-base")
+        model_type = "t5-11b"
+        t5 = T5ForConditionalGeneration.from_pretrained(model_type, return_dict=True)
+        t5_tokenizer = T5Tokenizer.from_pretrained(model_type)
         return t5, t5_tokenizer
 
     def get_data_source(self) -> str:
@@ -32,7 +33,7 @@ class MyTLDR(TLDR):
 app = L.LightningApp(
     L.app.components.LightningTrainerMultiNode(
         MyTLDR,
-        num_nodes=2,
-        cloud_compute=L.CloudCompute("gpu", disk_size=50),
+        num_nodes=10,
+        cloud_compute=L.CloudCompute("gpu-fast-multi", disk_size=50),
     )
 )
