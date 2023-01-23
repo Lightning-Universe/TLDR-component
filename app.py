079f061feaeb8a995f0cd52ac087fff799b87744
Justus Schock <justus.schock@posteo.de>
2023-01-23 19:50:32 +0100

    change recipe version

---
+++ app.py
@@ -0,0 +1,73 @@
+# !pip install git+https://github.com/Lightning-AI/LAI-TLDR-Component git+https://github.com/Lightning-AI/lightning-LLMs
+# !curl https://raw.githubusercontent.com/Shivanandroy/T5-Finetuning-PyTorch/main/data/news_summary.csv --create-dirs -o ${HOME}/data/summary/news.csv -C -
+
+import lightning as L
+import os; from transformers import T5ForConditionalGeneration, T5TokenizerFast as T5Tokenizer
+
+from lit_llms.tensorboard import (
+    DriveTensorBoardLogger,
+    MultiNodeLightningTrainerWithTensorboard,
+)
+
+from lai_tldr import TLDRDataModule, default_callbacks, predict, TLDRLightningModule
+
+
+class TLDR(L.LightningWork):
+    """Finetune on a text summarization task."""
+
+    def __init__(self, tb_drive, **kwargs):
+        super().__init__(**kwargs)
+        self.tensorboard_drive = tb_drive
+
+    def run(self):
+        # for huggingface/transformers
+        os.environ["TOKENIZERS_PARALLELISM"] = "false"
+
+        # --------------------
+        # CONFIGURE YOUR MODEL
+        # --------------------
+        model_type = "t5-base"
+        t5_tokenizer = T5Tokenizer.from_pretrained(model_type)
+        t5_model = T5ForConditionalGeneration.from_pretrained(model_type, return_dict=True)
+
+        lightning_module = TLDRLightningModule(t5_model, tokenizer=t5_tokenizer)
+
+        # -------------------
+        # CONFIGURE YOUR DATA
+        # -------------------
+        data_module = TLDRDataModule(os.path.expanduser("~/data/summary/news.csv"), t5_tokenizer)
+
+        # -----------------
+        # RUN YOUR TRAINING
+        # -----------------
+        strategy = "deepspeed_stage_3_offload" if L.app.utilities.cloud.is_running_in_cloud() else "ddp"
+        trainer = L.Trainer(
+            max_epochs=2, limit_train_batches=250,
+            precision=16, strategy=strategy,
+            callbacks=default_callbacks(), log_every_n_steps=1,
+            logger=DriveTensorBoardLogger(save_dir=".", drive=self.tensorboard_drive),
+        )
+        trainer.fit(lightning_module, data_module)
+
+        if trainer.global_rank == 0:
+            sample_text = (
+                "summarize: ML Ops platforms come in many flavors from platforms that train models to platforms "
+                "that label data and auto-retrain models. To build an ML Ops platform requires dozens of "
+                "engineers, multiple years and 10+ million in funding. The majority of that work will go into "
+                "infrastructure, multi-cloud, user management, consumption models, billing, and much more. "
+                "Build your platform with Lightning and launch in weeks not months. Focus on the workflow you want "
+                "to enable (label data then train models), Lightning will handle all the infrastructure, billing, "
+                "user management, and the other operational headaches."
+            )
+            predictions = predict(
+                lightning_module.to(trainer.strategy.root_device), sample_text
+            )
+            print("Input text:\n", sample_text)
+            print("Summarized text:\n", predictions[0])
+
+
+app = L.LightningApp(
+    MultiNodeLightningTrainerWithTensorboard(
+        TLDR, num_nodes=2, cloud_compute=L.CloudCompute("gpu-fast-multi", disk_size=50),
+    )
+)
---
+++ lai_tldr/data.py
@@ -0,0 +1,201 @@
+# MIT License
+#
+# Copyright (c) 2021 Shivanand Roy
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+"""
+Code in this file is based on https://github.com/Shivanandroy/simpleT5 by Shivanand Roy
+"""
+
+from typing import Optional
+
+import pandas as pd
+import os
+import urllib.request
+from lightning.pytorch import LightningDataModule, LightningModule
+from sklearn.model_selection import train_test_split
+from torch.utils.data import DataLoader, Dataset
+from transformers import PreTrainedTokenizer, T5ForConditionalGeneration
+
+
+class SummarizationDataset(Dataset):
+    """PyTorch Dataset class"""
+
+    def __init__(
+        self,
+        data: pd.DataFrame,
+        tokenizer: PreTrainedTokenizer,
+        source_max_token_len: int = 512,
+        target_max_token_len: int = 512,
+    ):
+        """
+        initiates a PyTorch Dataset Module for input data
+        Args:
+            data (pd.DataFrame): input pandas dataframe. Dataframe must have 2 column --> "source_text" and "target_text"
+            tokenizer (PreTrainedTokenizer): a PreTrainedTokenizer (T5Tokenizer, MT5Tokenizer, or ByT5Tokenizer)
+            source_max_token_len (int, optional): max token length of source text. Defaults to 512.
+            target_max_token_len (int, optional): max token length of target text. Defaults to 512.
+        """
+        self.tokenizer = tokenizer
+        self.data = data
+        self.source_max_token_len = source_max_token_len
+        self.target_max_token_len = target_max_token_len
+
+    def __len__(self):
+        """returns length of data"""
+        return len(self.data)
+
+    def __getitem__(self, index: int):
+        """returns dictionary of input tensors to feed into T5/MT5 model"""
+
+        data_row = self.data.iloc[index]
+        source_text = data_row["source_text"]
+
+        source_text_encoding = self.tokenizer(
+            source_text,
+            max_length=self.source_max_token_len,
+            padding="max_length",
+            truncation=True,
+            return_attention_mask=True,
+            add_special_tokens=True,
+            return_tensors="pt",
+        )
+
+        target_text_encoding = self.tokenizer(
+            data_row["target_text"],
+            max_length=self.target_max_token_len,
+            padding="max_length",
+            truncation=True,
+            return_attention_mask=True,
+            add_special_tokens=True,
+            return_tensors="pt",
+        )
+
+        labels = target_text_encoding["input_ids"]
+        labels[
+            labels == 0
+        ] = -100  # to make sure we have correct labels for T5 text generation
+
+        return dict(
+            source_text_input_ids=source_text_encoding["input_ids"].flatten(),
+            source_text_attention_mask=source_text_encoding["attention_mask"].flatten(),
+            labels=labels.flatten(),
+            labels_attention_mask=target_text_encoding["attention_mask"].flatten(),
+        )
+
+
+class TLDRDataModule(LightningDataModule):
+    """PyTorch Lightning data class"""
+
+    def __init__(
+        self,
+        data_source: str,
+        tokenizer: PreTrainedTokenizer,
+        batch_size: int = 8,
+        source_max_token_len: int = 512,
+        target_max_token_len: int = 128,
+        num_workers: int = min(os.cpu_count() - 1, 1),
+    ):
+        """
+        initiates a PyTorch Lightning Data Module
+        Args:
+            train_df (pd.DataFrame): training dataframe. Dataframe must contain 2 columns --> "source_text" & "target_text"
+            test_df (pd.DataFrame): validation dataframe. Dataframe must contain 2 columns --> "source_text" & "target_text"
+            tokenizer (PreTrainedTokenizer): PreTrainedTokenizer (T5Tokenizer, MT5Tokenizer, or ByT5Tokenizer)
+            batch_size (int, optional): batch size. Defaults to 4.
+            source_max_token_len (int, optional): max token length of source text. Defaults to 512.
+            target_max_token_len (int, optional): max token length of target text. Defaults to 512.
+        """
+        super().__init__()
+        self.data_source = data_source
+        self.train_df = None
+        self.val_df = None
+        self.test_df = None
+        self.batch_size = batch_size
+        self.tokenizer = tokenizer
+        self.source_max_token_len = source_max_token_len
+        self.target_max_token_len = target_max_token_len
+        self.num_workers = num_workers
+        self.train_dataset = None
+        self.val_dataset = None
+        self.test_dataset = None
+
+    def prepare_data(self):
+        pd.read_csv(self.data_source)
+
+    def setup(self, stage=None):
+        df = pd.read_csv(self.data_source)
+        df.head()
+
+        # simpleT5 expects dataframe to have 2 columns: "source_text" and "target_text"
+        df = df.rename(columns={"headlines": "target_text", "text": "source_text"})
+        df = df[["source_text", "target_text"]]
+
+        # T5 model expects a task related prefix: since it is a summarization task, we will add a prefix "summarize: "
+        df["source_text"] = "summarize: " + df["source_text"]
+        train_df, self.test_df = train_test_split(df, test_size=0.2)
+        # 0.25 of remaining is 0.2 of original size
+        self.train_df, self.val_df = train_test_split(train_df, test_size=0.25)
+        self.train_dataset = SummarizationDataset(
+            self.train_df,
+            self.tokenizer,
+            self.source_max_token_len,
+            self.target_max_token_len,
+        )
+        self.val_dataset = SummarizationDataset(
+            self.val_df,
+            self.tokenizer,
+            self.source_max_token_len,
+            self.target_max_token_len,
+        )
+
+        self.test_dataset = SummarizationDataset(
+            self.test_df,
+            self.tokenizer,
+            self.source_max_token_len,
+            self.target_max_token_len,
+        )
+
+    def train_dataloader(self):
+        """training dataloader"""
+        return DataLoader(
+            self.train_dataset,
+            batch_size=self.batch_size,
+            shuffle=True,
+            num_workers=self.num_workers,
+        )
+
+    def test_dataloader(self):
+        """test dataloader"""
+        return DataLoader(
+            self.test_dataset,
+            batch_size=self.batch_size,
+            shuffle=False,
+            num_workers=self.num_workers,
+        )
+
+    def val_dataloader(self):
+        """validation dataloader"""
+        return DataLoader(
+            self.val_dataset,
+            batch_size=self.batch_size,
+            shuffle=False,
+            num_workers=self.num_workers,
+        )
